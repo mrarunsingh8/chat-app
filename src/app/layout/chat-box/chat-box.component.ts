dd38226077e8f1e1b8206c38e5b0744feb34063c
Arun Singh <mrarunsingh8@gmail.com>
2018-05-20 22:53:20 +0530

    ....

--- src/app/layout/chat-box/chat-box.component.ts
+++ src/app/layout/chat-box/chat-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import * as io from 'socket.io-client';
 
 @Component({
   selector: 'app-chat-box',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-box.component.css']
 })
 export class ChatBoxComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('chatToolbar', {read: ElementRef}) chatToolbar: ElementRef;
+  @ViewChild('lastElm') lastElm: ElementRef;
+  url: any = 'http://localhost:3001/';
+  socket: any;
+  constructor() {}
 
   ngOnInit() {
+    this.socket = io(this.url);
+  }
+
+  ngOnDestroy(): void{
+    this.socket.disconnect();
+  }
+
+  ngAfterViewInit() {
+    this.scrollToElement();
+  }
+
+
+  public scrollToElement() {
+    console.log('Scrolled', this.chatToolbar);
+    this.chatToolbar.nativeElement.scrollTo(0, this.lastElm.nativeElement.offsetTop, 500);
   }
 
 }
---
+++ tools/app_socket.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser('SECRET_KEY_ENETER_HERE'));
+
+const port = process.env.PORT || 3001;
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  // allow preflight
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+app.use(logger('dev'));
+app.use(cors());
+
+app.get('/', function(req, res, next){
+  return res.end('Api working');
+});
+app.get('/api', function(req, res, next){
+  return res.end('Api working');
+});
+
+var io = require("socket.io").listen(app.listen(port));
+
+io.sockets.on('connection', function (socket) {
+  console.log("Connection Started on localhost:3001");
+});
